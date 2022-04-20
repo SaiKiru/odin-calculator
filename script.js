f6f9eba7d3617e2e04b95b5894219e7ffaa3ccd0
Anthony Aromin <menekoiskawaii@gmail.com>
2022-04-20 13:50:35 +0800

    Add operation caller function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const Operation = {
+  ADD: 0,
+  SUBTRACT: 1,
+  MULTIPLY: 2,
+  DIVIDE: 3,
+};
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +19,13 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(operation, num1, num2) {
+  switch (operation) {
+    case Operation.ADD: return add(num1, num2);
+    case Operation.SUBTRACT: return subtract(num1, num2);
+    case Operation.MULTIPLY: return multiply(num1, num2);
+    case Operation.DIVIDE: return divide(num1, num2);
+  }
 }
