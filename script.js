b56feb362a4f02d0b352e89503afef9f8d38d4f9
Anthony Aromin <menekoiskawaii@gmail.com>
2022-04-20 19:54:14 +0800

    Add display logic

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ const Operation = {
   DIVIDE: 3,
 };
 
+let calculatorButtons = document.querySelectorAll('button');
+let display = document.querySelector('#calculator-display');
+let displayStr = '';
+
+calculatorButtons.forEach(button => {
+  let func;
+
+  switch (button.textContent) {
+    case 'CLS':
+      func = () => clearDisplay();
+      break;
+    case '=':
+      func = () => compute();
+      break;
+    default:
+      func = () => addToDisplay(button.textContent);
+  }
+
+  button.addEventListener('click', func);
+});
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -28,4 +49,28 @@ function operate(operation, num1, num2) {
     case Operation.MULTIPLY: return multiply(num1, num2);
     case Operation.DIVIDE: return divide(num1, num2);
   }
+}
+
+function compute() {
+
+}
+
+function clearDisplay() {
+  displayStr = '';
+  setDisplay(displayStr);
+}
+
+function addToDisplay(symbol) {
+  if (!displayStr) {
+    displayStr = '';
+  }
+  displayStr = displayStr.concat(symbol);
+  setDisplay(displayStr);
+}
+
+function setDisplay(str) {
+  if (!displayStr) {
+    str = '0';
+  }
+  display.textContent = str;
 }
